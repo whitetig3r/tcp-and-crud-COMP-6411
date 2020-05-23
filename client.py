ff6f27eb422da8f066e90abdfd4f70354ddc294b
Warren White <wwhite@samasource.org>
2020-05-22 20:59:06 -0400

    add refactors and error handling

--- client.py
+++ client.py
@@ -4,61 +4,98 @@ HOST = "localhost"
 PORT = 9999
 SEP = "$%^#"
 
+# network helper
 def make_request(params = "print_report|"):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((HOST, PORT))
-        sock.sendall(bytes(params + "\n", "utf-8"))
-        received = str(sock.recv(1024), "utf-8")
-        print("\n-- Server Replied with -- \n")
-        print(received)
-        print("\n-- END OF RESPONSE --")
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.connect((HOST, PORT))
+            sock.sendall(bytes(params + "\n", "utf-8"))
+            received = str(sock.recv(1024), "utf-8")
+            print("\n-- Server Replied with -- \n")
+            print(received)
+            print("\n-- END OF RESPONSE --")
+    except:
+        print("ERROR: NETWORK ERROR! FAILED TO MAKE REQUEST.")        
+
+# input helpers
+def safe_input(prompt):
+    try:
+        while(True):
+            inp = input(prompt)       
+            if (SEP in inp) or ('|' in inp):
+                print('ERROR: Input cannot contain the special characters %s or | as a substring'%(SEP))
+            else:
+                return inp
+    except:
+        print("ERROR: FAILED TO TAKE INPUT!")        
 
 def get_age_input():
-    while(True):
-        c_age = input("Enter a valid age:").strip()
-        if not c_age:
-            return c_age
-        if(c_age.startswith('-')):
-            print("ERR: Negative age is INVALID!")
-            continue
-        if(c_age.isdigit() and int(c_age) < 150):
-            return c_age
-        else:
-            print("ERR: Age is not a number OR age is too large")
+    try: 
+        while(True):
+            c_age = safe_input("Enter a valid age:").strip()
+            if (not c_age) or (c_age.isdigit() and int(c_age) < 150):
+                return c_age
+            else:
+                print("ERROR: Age is not a number OR age is too large")
+    except:
+        print("ERROR: FAILED TO TAKE AGE INPUT!")                
 
+# core processing methods
 def c_find():
-    c_name = input("Enter name of customer to find:").strip()
-    make_request("find|"+c_name)
+    try:
+        c_name = safe_input("Enter name of customer to find:").strip()
+        make_request("find|"+c_name)
+    except:
+        print("ERROR: FAILED TO PROCESS FIND")        
 
 def c_add():
-    c_name = input("Enter first name:").strip()
-    c_age = get_age_input()
-    c_address = input("Enter address:").strip()
-    c_phone = input("Enter phone:").strip()
-    make_request("add|"+SEP.join([c_name,c_age,c_address,c_phone]))
+    try:
+        c_name = safe_input("Enter first name:").strip()
+        c_age = get_age_input()
+        c_address = safe_input("Enter address:").strip()
+        c_phone = safe_input("Enter phone:").strip()
+        make_request("add|"+SEP.join([c_name,c_age,c_address,c_phone]))
+    except:
+        print("ERROR: FAILED TO PROCESS INSERT")        
 
 def c_delete():
-    c_name = input("Enter name of customer to delete:").strip()
-    make_request("delete|"+c_name)
+    try:
+        c_name = safe_input("Enter name of customer to delete:").strip()
+        make_request("delete|"+c_name)
+    except:
+        print("ERROR: FAILED TO PROCESS DELETE")        
 
 def c_update_age():
-    c_name = input("Enter name of customer to update:").strip()
-    c_age = get_age_input()
-    make_request("update_age|"+SEP.join([c_name,c_age]))
+    try:
+        c_name = safe_input("Enter name of customer to update:").strip()
+        c_age = get_age_input()
+        make_request("update_age|"+SEP.join([c_name,c_age]))
+    except:
+        print("ERROR: FAILED TO PROCESS UPDATE AGE")        
 
 def c_update_address():
-    c_name = input("Enter name of customer to update:").strip()
-    c_address = input("Enter updated address:").strip()
-    make_request("update_address|"+SEP.join([c_name,c_address]))
+    try:
+        c_name = safe_input("Enter name of customer to update:").strip()
+        c_address = safe_input("Enter updated address:").strip()
+        make_request("update_address|"+SEP.join([c_name,c_address]))
+    except:
+        print("ERROR: FAILED TO PROCESS UPDATE ADDRESS")        
 
 def c_update_phone():
-    c_name = input("Enter name of customer to update:").strip()
-    c_phone = input("Enter updated phone:").strip()
-    make_request("update_phone|"+SEP.join([c_name,c_phone]))
+    try:
+        c_name = safe_input("Enter name of customer to update:").strip()
+        c_phone = safe_input("Enter updated phone:").strip()
+        make_request("update_phone|"+SEP.join([c_name,c_phone]))
+    except:
+        print("ERROR: FAILED TO PROCESS UPDATE PHONE")
 
 def c_print_report():
-    make_request()
+    try:
+        make_request()
+    except:
+        print("ERROR: FAILED TO PROCESS PRINT REPORT")        
                
+# main loop and vars
 MENU_STRING = """
 
 1. Find customer 
@@ -74,32 +111,35 @@ MENU_STRING = """
 
 print ("Python DB Menu")
 
-while(True):
-    print(MENU_STRING)
-    selected_option = input("Select:").strip()
-    
-    if not selected_option.isdigit():
-        print("ERR: INVALID OPTION ENTERED!")
-        continue
-    else:
-        selected_option = int(selected_option)
+try:
+    while(True):
+        print(MENU_STRING)
+        selected_option = input("Select:").strip()
+        
+        if not selected_option.isdigit():
+            print("ERROR: INVALID OPTION ENTERED!")
+            continue
+        else:
+            selected_option = int(selected_option)
 
-    if selected_option == 1:
-        c_find()
-    elif selected_option == 2:
-        c_add()  
-    elif selected_option == 3:
-        c_delete() 
-    elif selected_option == 4:
-        c_update_age()
-    elif selected_option == 5:
-        c_update_address()
-    elif selected_option == 6:
-        c_update_phone()
-    elif selected_option == 7:
-        c_print_report()
-    elif selected_option == 8:
-        print("Good bye!")
-        break 
-    else:
-        print("Please Enter a valid option!")                           
+        if selected_option == 1:
+            c_find()
+        elif selected_option == 2:
+            c_add()  
+        elif selected_option == 3:
+            c_delete() 
+        elif selected_option == 4:
+            c_update_age()
+        elif selected_option == 5:
+            c_update_address()
+        elif selected_option == 6:
+            c_update_phone()
+        elif selected_option == 7:
+            c_print_report()
+        elif selected_option == 8:
+            print("Good bye!")
+            break 
+        else:
+            print("Please Enter a valid option!")                           
+except:                        
+    print("ERROR: UNRECOGNIZED ERROR!")
--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 import socketserver
 import os
+import sys
+
+DATA_FILE = 'data.txt'
+SEP = "$%^#"
 
 customer_tuples = {}
 
@@ -8,119 +12,172 @@ ERR_ALREADY_EXIST = "Customer already exists!"
 ERR_NOT_FOUND = "Customer not found!" 
 
 class RequestHandler(socketserver.BaseRequestHandler):
-    # helper methods
-    def disp_customer(self, customer):
-        return "{}|{}|{}|{}".format(customer['first_name'], customer['age'], customer['address'], customer['phone_no'])
+    # helper methods  
+    def disp_customer_pretty(self, customer):
+        try:
+            return "Name: {} || Age:  {} || Addr.: {} || Ph.No.: {}".format(customer['first_name'], customer['age'], customer['address'], customer['phone_no'])
+        except:
+            print("ERROR: FAILED TO RENDER CUSTOMER TUPLE AS UTF-8 STRING")           
 
     # processing methods
     def process_find(self, arg_list):
-        if arg_list[0].strip() in customer_tuples.keys():
-            return self.disp_customer(customer_tuples[arg_list[0].strip()])
-        else:
-            return ERR_NOT_FOUND  
+        try:
+            arg_list[0] = arg_list[0].strip().lower()
+            if arg_list[0] in customer_tuples.keys():
+                return self.disp_customer_pretty(customer_tuples[arg_list[0].strip()])
+            else:
+                return ERR_NOT_FOUND  
+        except:
+            print("ERROR: FAILED TO PROCESS FIND OPERATION")        
 
     def process_add(self, arg_list):
-        customer_to_add = {
-            "first_name" : arg_list[0].strip(),
-            "age": arg_list[1].strip(),
-            "address": arg_list[2].strip(),
-            "phone_no": arg_list[3].strip()
-        }
-        if customer_to_add['first_name'] not in customer_tuples.keys():
-            customer_tuples[arg_list[0].strip()] = customer_to_add
-            return "Added tuple successfully -- {}".format(self.disp_customer(customer_to_add))
-        else:
-            return ERR_ALREADY_EXIST   
+        try:
+            arg_list[0] = arg_list[0].strip()
+            customer_to_add = {
+                "first_name" : arg_list[0],
+                "age": arg_list[1].strip(),
+                "address": arg_list[2].strip(),
+                "phone_no": arg_list[3].strip()
+            }
+            if arg_list[0].lower() not in customer_tuples.keys():
+                customer_tuples[arg_list[0].lower()] = customer_to_add
+                return "Added tuple successfully -- {}".format(self.disp_customer_pretty(customer_to_add))
+            else:
+                return ERR_ALREADY_EXIST   
+        except:
+            print("ERROR: FAILED TO PROCESS INSERT OPERATION")        
     
     def process_delete(self, arg_list):
-        if arg_list[0].strip() in customer_tuples.keys():
-            customer_tuples.pop(arg_list[0].strip())
-            return "Successfully Deleted Customer with name -- {}".format(arg_list[0].strip())    
-        else:
-            return ERR_DOES_NOT_EXIST 
+        try:
+            arg_list[0] = arg_list[0].strip().lower()
+            if arg_list[0] in customer_tuples.keys():
+                customer_tuples.pop(arg_list[0])
+                return "Successfully Deleted Customer with name -- {}".format(arg_list[0])    
+            else:
+                return ERR_DOES_NOT_EXIST 
+        except:
+            print("ERROR: FAILED TO PROCESS DELETE OPERATION")        
 
     def process_update_age(self, arg_list):
-        if arg_list[0].strip() in customer_tuples.keys():
-            customer_tuples[arg_list[0].strip()]['age'] = arg_list[1].strip()
-            return "Successfully Updated Age to '{}' for Customer with Name -- {}".format(arg_list[1].strip(),arg_list[0].strip())    
-        else:
-            return ERR_NOT_FOUND  
+        try:
+            arg_list[0] = arg_list[0].strip().lower() 
+            if arg_list[0] in customer_tuples.keys():
+                customer_tuples[arg_list[0]]['age'] = arg_list[1].strip()
+                return "Successfully Updated Age to '{}' for Customer with Name -- {}".format(arg_list[1].strip(),arg_list[0])    
+            else:
+                return ERR_NOT_FOUND  
+        except:
+            print("ERROR: FAILED TO PROCESS UPDATE_AGE OPERATION")        
 
     def process_update_address(self, arg_list):
-        if arg_list[0].strip() in customer_tuples.keys():
-            customer_tuples[arg_list[0].strip()]['address'] = arg_list[1].strip() 
-            return "Successfully Updated Address to '{}' for Customer with Name -- {}".format(arg_list[1].strip(),arg_list[0].strip())    
-        else:
-            return ERR_NOT_FOUND  
+        try:
+            arg_list[0] = arg_list[0].strip().lower() 
+            if arg_list[0] in customer_tuples.keys():
+                customer_tuples[arg_list[0]]['address'] = arg_list[1].strip() 
+                return "Successfully Updated Address to '{}' for Customer with Name -- {}".format(arg_list[1].strip(),arg_list[0])    
+            else:
+                return ERR_NOT_FOUND  
+        except:
+            print("ERROR: FAILED TO PROCESS UPDATE_ADDRESS OPERATION")        
 
     def process_update_phone(self, arg_list):
-        if arg_list[0].strip() in customer_tuples.keys():
-            customer_tuples[arg_list[0].strip()]['phone_no'] = arg_list[1].strip() 
-            return "Successfully Updated Phone Number to '{}' for Customer with Name -- {}".format(arg_list[1].strip(),arg_list[0].strip())    
-        else:
-            return ERR_NOT_FOUND  
+        try:
+            arg_list[0] = arg_list[0].strip().lower() 
+            if arg_list[0] in customer_tuples.keys():
+                customer_tuples[arg_list[0]]['phone_no'] = arg_list[1].strip() 
+                return "Successfully Updated Phone Number to '{}' for Customer with Name -- {}".format(arg_list[1].strip(),arg_list[0])    
+            else:
+                return ERR_NOT_FOUND  
+        except:
+            print("ERROR: FAILED TO PROCESS UPDATE_PHONE OPERATION")        
 
     def process_print_report(self, arg_list):
-        return "\n".join(list(map(self.disp_customer, customer_tuples.values())))
+        try:
+            customer_list = list(map(self.disp_customer_pretty, customer_tuples.values()))
+            return "\n".join(sorted(customer_list))
+        except:
+            print("ERROR: FAILED TO PROCESS PRINT_REPORT OPERATION")    
 
     def parse_and_process(self):
-        req = self.data.split("|")
-        op = req[0]
-        arg_list = req[1].split("$%^#")
-        # names should be case insensitive
-        arg_list[0] = arg_list[0].lower()
-        if op == "find":
-            return self.process_find(arg_list)
-        elif op == "add":
-            return self.process_add(arg_list)
-        elif op == "delete":      
-            return self.process_delete(arg_list)
-        elif op == "update_age":
-            return self.process_update_age(arg_list)
-        elif op == "update_address":
-            return self.process_update_address(arg_list)      
-        elif op == "update_phone":
-            return self.process_update_phone(arg_list)  
-        elif op == "print_report":
-            return self.process_print_report(arg_list)
-        else:
-            return "ERROR: UNRECOGNIZED OPERATION!"    
+        try:
+            req = self.data.split("|")
+            op = req[0]
+            arg_list = req[1].split(SEP)
+            if op == "find":
+                return self.process_find(arg_list)
+            elif op == "add":
+                return self.process_add(arg_list)
+            elif op == "delete":      
+                return self.process_delete(arg_list)
+            elif op == "update_age":
+                return self.process_update_age(arg_list)
+            elif op == "update_address":
+                return self.process_update_address(arg_list)      
+            elif op == "update_phone":
+                return self.process_update_phone(arg_list)  
+            elif op == "print_report":
+                return self.process_print_report(arg_list)
+            else:
+                return "ERROR: UNRECOGNIZED OPERATION!"   
+        except: 
+            print("ERROR: BAD REQUEST MADE BY CLIENT")         
 
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print("{} made a request!".format(self.client_address[0]))
-        self.data = self.data.decode("utf-8")
-        ret_message = self.parse_and_process()
-        self.request.sendall(ret_message.encode("utf-8"))
+        try:
+            self.data = self.request.recv(1024).strip()
+            print("{} made a request!".format(self.client_address[0]))
+            self.data = self.data.decode("utf-8")
+            ret_message = self.parse_and_process()
+            self.request.sendall(ret_message.encode("utf-8"))
+        except:
+            print("ERROR: FAILED TO HANDLE THE REQUEST!")    
 
-def store_value_in_hash(c_tuple):   
-    c_tuple = c_tuple.split("|")  
+def store_value_in_hash(c_tuple):
+    try:   
+        c_tuple = c_tuple.split("|")  
+    
+        if len(c_tuple) != 4:
+            print("ERROR: BAD FORMAT FOR TUPLE! MOVING TO NEXT TUPLE...")
+            return
+        
+        if not c_tuple[0].strip(): 
+            return
 
-    if not c_tuple[0].strip(): 
-        return
-
-    customer_tuples[c_tuple[0].strip().lower()] = {
-        "first_name" : c_tuple[0].strip().lower(),
-        "age": c_tuple[1].strip(),
-        "address": c_tuple[2].strip(),
-        "phone_no": c_tuple[3].strip()
-    }
+        customer_tuples[c_tuple[0].strip().lower()] = {
+            "first_name" : c_tuple[0].strip(),
+            "age": c_tuple[1].strip(),
+            "address": c_tuple[2].strip(),
+            "phone_no": c_tuple[3].strip()
+        }
+    except:
+        print("ERROR: FAILED TO STORE TUPLE IN MEMORY HASH")    
 
 def load_db():
-    data_file = open('data.txt','r')
-    c_tuples = data_file.readlines()
-    for c_tuple in c_tuples: 
-        store_value_in_hash(c_tuple.strip())  
-    data_file.close()    
+    with open(DATA_FILE, 'r') as data_file: 
+        try:
+            c_tuples = data_file.readlines()
+            for c_tuple in c_tuples: 
+                store_value_in_hash(c_tuple.strip())  
+            data_file.close()
+        except OSError:
+            print("ERROR: COULD NOT OPEN 'data.txt'. Ensure it exists in your path")    
+            sys.exit()
+        except:
+            print("ERROR: UNRECOGNIZED ERROR")            
 
 def init_server():
     HOST, PORT = "localhost", 9999
 
     load_db()
 
-    with socketserver.TCPServer((HOST, PORT), RequestHandler) as server:
-        print("Server is active on port 9999 ...")
-        server.serve_forever()
+    try:
+        with socketserver.TCPServer((HOST, PORT), RequestHandler) as server:
+            print("Server is active on port 9999 ...")
+            server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nSuccessfully shutdown server!")            
+    except:
+        print("ERROR: INTERNAL SERVER ERROR (UNABLE TO SERVE)")        
 
 
 init_server()        
